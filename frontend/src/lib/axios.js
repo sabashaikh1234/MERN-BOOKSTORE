3f5f8c7841f2553325af6250f87dbe6bd9ef8ce0
sabashaikh1234 <chmcs2026.saba.shaikh@gmail.com>
2026-02-25 08:41:48 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const api=axios.create({
-    baseURL: 'https://mern-bookstore-sa31.onrender.com/books'
+    baseURL: 'https://mern-bookstore-sa31.onrender.com'
 })
 export default api
